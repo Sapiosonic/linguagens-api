45d94d04c246f9ca225878ae40d631f2898c5e85
Radamés Rodrigues <radames.idiomas@gmail.com>
2022-07-23 23:43:11 -0300

    "Adding dependencies"

--- src/main/java/br/com/alura/linguagens/api/Linguagem.java
+++ src/main/java/br/com/alura/linguagens/api/Linguagem.java
@@ -1,7 +1,13 @@
 package br.com.alura.linguagens.api;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "principaisLinguagens")
 public class Linguagem {
 
+    @Id
+    private String id;
     private String title;
     private String image;
     private int ranking;
@@ -12,6 +18,14 @@ public class Linguagem {
         this.ranking = ranking;
     }
 
+    public Linguagem() {
+
+    }
+
+    public String getId(){
+        return id;
+    }
+    
     public String getTitle(){
         return title;
     }
